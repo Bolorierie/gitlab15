72a93be2ac15d6c40b44a255d34ef9fe8e929ed3
bolorierie <lynklynk56@gmail.com>
2025-05-09 00:14:13 +0800

    Conflicting Javadoc change in feature branch

---
+++ calculator/calculator-backend/src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -0,0 +1,10 @@
+package labxx.sict.must.edu.mn;
+
+\/**
+ * This class provides a method to multiply two numbers.
+ */
+public class Multiplication {
+  public double multiply(double a, double b) {
+    return a * b;
+  }
+}
